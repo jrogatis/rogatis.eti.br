af9a2fb3f0626aaf62efd52c98532581c95f1e2d
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-22 11:22:20 -0200

    local.env set

--- server/api/contactForm/contactForm.controller.js
+++ server/api/contactForm/contactForm.controller.js
@@ -20,50 +20,18 @@ mailer.extend(app, {
   port: 465, // port for secure SMTP
   transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
   auth: {
-    user: 'jrogatis@gmail.com', //process.env.GMAIL_USER
-    pass: 'Nick0504' //process.env.GMAIL_SECRET
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_SECRET
   }
 });
 
 app.set('views', __dirname + '/');
 app.set('view engine', 'pug');
 
-function respondWithResult(res, statusCode) {
-  statusCode = statusCode || 200;
-  return function(entity) {
-    if(entity) {
-      return res.status(statusCode).json(entity);
-    }
-    return null;
-  };
-}
-
-function patchUpdates(patches) {
-  return function(entity) {
-    try {
-      jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
-      return Promise.reject(err);
-    }
-
-    return entity.save();
-  };
-}
-
-function removeEntity(res) {
-  return function(entity) {
-    if(entity) {
-      return entity.remove()
-        .then(() => {
-          res.status(204).end();
-        });
-    }
-  };
-}
 
 function handleEntityNotFound(res) {
-  return function(entity) {
-    if(!entity) {
+  return function (entity) {
+    if (!entity) {
       res.status(404).end();
       return null;
     }
@@ -73,31 +41,31 @@ function handleEntityNotFound(res) {
 
 function handleError(res, statusCode) {
   statusCode = statusCode || 500;
-  return function(err) {
+  return function (err) {
     res.status(statusCode).send(err);
   };
 }
 
 function handleSendEmail(res, msg) {
-  console.log(msg, msg.email)
+  console.log('no email', process.env)
   app.mailer.send('email', {
-      to: msg.email,
-      subject: 'Your contact for Jean', // REQUIRED.
-      otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
-    }, function (err) {
-      if (err) {
-        // handle error
-        console.log(err);
-        res.send('There was an error sending the email');
-        return;
-      }
-      res.send('Email Sent');
-    });
+    to: msg.email,
+    subject: 'Your contact for Jean', // REQUIRED.
+    otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
+  }, function (err) {
+    if (err) {
+      // handle error
+      console.log(err);
+      res.send('There was an error sending the email');
+      return;
+    }
+    res.send('Email Sent');
+  });
 }
 
 // Creates a new ContactForm in the DB
 export function create(req, res) {
   return ContactForm.create(req.body)
-    .then(handleSendEmail(res,req.body))
+    .then(handleSendEmail(res, req.body))
 
 }
