1fd39c3392203f382ebc45d87878469749ab6ba9
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-23 18:57:36 -0200

    iniitial blog

--- client/app/blog/blog.component.js
+++ client/app/blog/blog.component.js
@@ -4,6 +4,7 @@ import routing from './blog.routes';
 import ngMdIcons from 'angular-material-icons';
 import ngMessages from 'angular-messages';
 import ngAria from 'angular-aria';
+import ngMaterial from 'angular-material';
 
 export class BlogController {
 
@@ -13,18 +14,28 @@ export class BlogController {
     this.$scope = $scope;
     this.socket = socket;
     this.$mdDialog = $mdDialog;
+    this.listPosts = [{
+        title: ' Type Script or not ?'
+      ,
+      text: `t vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidemrerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+          quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+          Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+          sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+          voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.`
+    }];
+
   }
 
   $onInit() {
-    this.$http.get('/api/posts')
+    /*this.$http.get('/api/posts')
       .then(response => {
         this.listPosts = response.data;
         this.socket.syncUpdates('posts', this.listPosts);
-      });
+      });*/
   }
 }
 
-export default angular.module('rogatisEtiBrApp.blog', [ngRoute, ngMdIcons, ngMessages, ngAria])
+export default angular.module('rogatisEtiBrApp.blog', [ngRoute, ngMdIcons, ngMessages, ngAria, ngMaterial])
   .config(routing)
   .component('blog', {
     template: require('./blog.pug'),
