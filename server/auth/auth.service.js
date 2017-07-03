690b6ff4ee143aa3bf91eba1928c337353532354
Jean Philip de Rogatis <jrogatis@conecta.net.br>
2017-07-03 05:44:21 -0300

    style

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -60,20 +60,19 @@ export const hasRole = roleRequired => {
 /**
  * Returns a jwt token signed by the app secret
  */
-export function signToken(id, role) {
-  return jwt.sign({ _id: id, role }, config.secrets.session, {
-    expiresIn: 60 * 60 * 5
-  });
-}
+export const signToken = (id, role) => jwt.sign({ _id: id, role }, config.secrets.session, {
+  expiresIn: 60 * 60 * 5
+});
+
 
 /**
  * Set token cookie directly for oAuth strategies
  */
-export function setTokenCookie(req, res) {
+export const setTokenCookie = (req, res) => {
   if (!req.user) {
     return res.status(404).send('It looks like you aren\'t logged in, please try again.');
   }
   const token = signToken(req.user._id, req.user.role);
   res.cookie('token', token);
   res.redirect('/');
-}
+};
